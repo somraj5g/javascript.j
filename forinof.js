52270368ed33c78a95fb9727428b78bdda440458
somraj5g <somrajusk2005@gmail.com>
2025-06-29 19:20:15 +0530

    Update forinof.js

---
+++ forinof.js
@@ -0,0 +1,20 @@
+const states = [
+    "Bihar",
+    "Jharkhand",
+    "Madhya Pradesh",
+    "Goa",
+    1995,
+    "Chandigarh",
+    "Manipur",
+    "Sikkim",
+];
+
+const r = {
+    Name: "Sara",
+    Age: 30,
+    Country: "Canada",
+    State: "Ontario",
+};
+for (const h in r) {
+    console.log(`key is: ${h} and value is: ${r[h]}`);
+};
