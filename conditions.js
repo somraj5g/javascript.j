026f970573b0fbda600d2b77aa80747838e31249
somraj5g <somrajusk2005@gmail.com>
2025-06-29 19:19:16 +0530

    Update conditions.js

---
+++ conditions.js
@@ -0,0 +1,13 @@
+const a = 35;
+const b = 25;
+const c = 15;
+
+if ((a > b) && (a > c)) {
+    console.log("a is biggest");
+}
+else if (b > a && b > c) {
+    console.log("b is biggest");
+}
+else {
+    console.log("c is biggest");
+}
