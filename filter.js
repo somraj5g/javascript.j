b73f260df4e3b9848eb212e240f630e0575b6f8c
somraj5g <somrajusk2005@gmail.com>
2025-06-29 19:19:36 +0530

    Update filter.js

---
+++ filter.js
@@ -0,0 +1,6 @@
+var c = new Array(9, 10, 11, 12, 13, 14, 15, 16);
+console.log(c.fill("hello", 2, 6));
+
+var a = [22, 33, 45, 28, 90, 110, 150];
+var r = a.filter((e) => (e > 50));
+console.log(r);
