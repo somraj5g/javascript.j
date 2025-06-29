970f860dc061ba6df63323335896453cd2c5bdb8
somraj5g <somrajusk2005@gmail.com>
2025-06-29 19:19:55 +0530

    Update foreach.js

---
+++ foreach.js
@@ -0,0 +1,12 @@
+const states = [
+    "Andhra Pradesh",
+    "Telangana",
+    "Punjab",
+    "Gujarat",
+    2024,
+    "Rajasthan",
+    "Assam",
+    "Odisha",
+];
+
+states.forEach((e) => (console.log(e)));
